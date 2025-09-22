4b220493806a5ba0d2aa53b344788ed604bfbd7f
LUCAS HENRIQUE DOS SANTOS <86979899+LucasSantosTaqua2@users.noreply.github.com>
2025-09-22 13:51:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -16,9 +16,11 @@ load_dotenv()
 # Cria a instância do FastAPI
 app = FastAPI()
 
+FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:4200")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://prj-cotd-front.vercel.app/"],
+    allow_origins=[FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
