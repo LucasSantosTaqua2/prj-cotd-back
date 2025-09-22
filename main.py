4c641f1b4c1eba16c8ee4a45d5674c4d6ce70518
LUCAS HENRIQUE DOS SANTOS <86979899+LucasSantosTaqua2@users.noreply.github.com>
2025-09-22 13:46:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
+    allow_origins=["https://prj-cotd-front.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
